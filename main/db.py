d6828b99e2d8bcc7ae636133eb13bc8dc3a1965d
JG1511 <joaotsi2024@gmail.com>
2025-05-30 15:55:23 -0300

    criação da função de incialização

--- main/db.py
+++ main/db.py
@@ -21,3 +21,22 @@ def close_db(e=None):
     db = g.pop('db', None) # Remove a conexão do contexto global g
     if db is not None:
         db.close()
+
+def init_db(): # função para inicializar o banco de dados
+    db = get_db() # Obtém a conexão com o banco de dados
+    with current_app.open_resource('tables.sql') as f: # Abre o arquivo SQL que contém os comandos para criar as tabelas. o current_app.open_resource() é usado para abrir arquivos que estão dentro do diretório da aplicação Flask, como templates, arquivos estáticos ou scripts SQL.
+        db.executescript(f.read().decode('utf8')) # Executa os comandos SQL para criar as tabelas
+
+@click.command('init-db') # Cria um comando de linha de comando para inicializar o banco de dados
+def init_db_command(): # Limpa o banco de dados e cria as tabelas
+    init_db()
+    click.echo('Banco de dados inicializado.')
+
+sqlite3.register_converter( # Registra um conversor para o tipo datetime
+    "timestamp",lambda v: datetime.fromisoformat(v.decode())
+)
+ 
+def init_app(app):
+
+    app.teardown_appcontext(close_db)  # Registra a função close_db para ser chamada quando o contexto da aplicação for encerrado
+    app.cli.add_command(init_db_command)  # Adiciona o comando init-db ao CLI da aplicação

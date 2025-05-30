baf1cf9758a52ec63daffe108c2f10a5a450d934
JG1511 <joaotsi2024@gmail.com>
2025-05-30 15:31:42 -0300

    implementação das tabelas

--- main/db.py
+++ main/db.py
@@ -1,15 +1,16 @@
 import sqlite3
-from datatime import datetime
+from datetime import datetime
 
 import click
 from flask import current_app, g
 
+
 def get_db():
     """Conecta ao banco de dados SQLite e retorna a conexão."""
-    if 'db' not in g:
+    if 'db' not in g: # o g é um objeto global do Flask que persiste durante a requisição,ou seja, ele armazena dados que podem ser acessados em qualquer parte da aplicação durante o ciclo de vida de uma requisição.
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config['DATABASE'], # Obtém o caminho do banco de dados a partir da configuração da aplicação
+            detect_types=sqlite3.PARSE_DECLTYPES # Permite o uso de tipos de dados como datetime, ou seja, permite acessar colunas do tipo datetime diretamente como objetos datetime em vez de strings.
         )
         g.db.row_factory = sqlite3.Row  # Permite acessar colunas por nome
 
